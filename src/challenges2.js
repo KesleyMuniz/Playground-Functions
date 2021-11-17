d77c0dacc9e9beacc8d903133f80ac49aa9018ff
kesley Muniz <contato.kgmstudios@hotmail.com>
2021-11-16 23:20:30 -0300

    Requisito 11 ajustado para melhor entendimento

--- src/challenges2.js
+++ src/challenges2.js
@@ -7,10 +7,10 @@ function techList(tec, name) {
   if (ordenado.length === 0) {
     return 'Vazio!';
   }
-  for (let i in ordenado) { 
-      tecnologias.push( {
+  for (let i in ordenado) {
+    tecnologias.push({
       tech: ordenado[i],
-      name, 
+      name,
     });
   }
   return tecnologias;
@@ -19,39 +19,27 @@ function techList(tec, name) {
 // Desafio 11
 function generatePhoneNumber(array) {
   // seu código aqui
-  let zerado = array
-  if (zerado.length === 0){
-      return "Array com tamanho incorreto."
+  let limitsize = array
+  if (limitsize.length === 0 || array.length != 11) {
+    return "Array com tamanho incorreto."
   }
-  for (let i = 0; i < array.length; i += 1){
-  if (array.length != 11){
-          return "Array com tamanho incorreto."
-// Nao pode ser menor que 0.
-      } for (let i in array ){
-          if (array[i] < 0) {
-              return "não é possível gerar um número de telefone com esses valores"
-          } 
-      } 
+  for (let i in array) {
+    if (array[i] > 9 || array[i] < 0) {
+      return 'não é possível gerar um número de telefone com esses valores'
+    } else {
+      let strainer = array.filter(repet => (repet) === array[i])
+      if (strainer.length > 2) {
+        return "não é possível gerar um número de telefone com esses valores"
+      }
+    }
   }
-// Não pode ser maior que 9.
-  for (let i = 0; i < array.length; i += 1){
-      if (array[i] > 9){
-          return 'não é possível gerar um número de telefone com esses valores'
-// Não pode repetir 3 vezes.
-      } else {
-          let filtro = array.filter(repetidos => (repetidos) === array[i]) 
-          if (filtro.length  > 2){
-          return "não é possível gerar um número de telefone com esses valores"
-  }}
+  let telefone = array;
+  let ddd = telefone.slice(0, 2).join('')
+  let fistnumber = telefone.slice(2, 7).join('')
+  let secondnumber = telefone.slice(7, 11).join('')
+  return array = `(${ddd}) ${fistnumber}-${secondnumber}`;
 }
 
-if (array.length === []){
-  return "Array com tamanho incorreto."
-}
-// Numero correto
-let telefone = array;
-return `(${telefone[0]}${telefone[1]}) ${telefone[2]}${telefone[3]}${telefone[4]}${telefone[5]}${telefone[6]}-${telefone[7]}${telefone[8]}${telefone[9]}${telefone[10]}`;
-}  
 
 
 // Desafio 12
